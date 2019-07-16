e98765382b9a19c50c9d7e381728e1bbe57f609e
victor.magarlamov <victor.magarlamov@gmail.com>
2019-07-16 21:42:52 +0300

    Updates

---
+++ precache-manifest.f5a0bf27ae4159a175cbd45143598af1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b117638c5916c02078fc9fb147bad38d",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "8264af3fdd62042f59b0",
+    "url": "/tetris/static/css/main.4f0219e0.chunk.css"
+  },
+  {
+    "revision": "c956edce98b98bbb5541",
+    "url": "/tetris/static/js/2.c0355920.chunk.js"
+  },
+  {
+    "revision": "8264af3fdd62042f59b0",
+    "url": "/tetris/static/js/main.8e986d9c.chunk.js"
+  },
+  {
+    "revision": "e9e58439d3b8c64431c2",
+    "url": "/tetris/static/js/runtime~main.61707ba2.js"
+  }
+]);
